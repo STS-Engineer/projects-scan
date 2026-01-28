7a797d3764144b771b9d6d685793f5ed3c49de3e
STS-Engineer <administration.sts@avocarbon.com>
2026-01-28 14:04:37 +0100

    Update scanConfig.js

--- config/scanConfig.js
+++ config/scanConfig.js
@@ -3,12 +3,6 @@ module.exports = [
     db:'CUSTOMER_INTERACTION',
     table: 'conversations',
     reportName: 'Customer Interaction  Report',
-    recipients: ['mootaz.farwa@avocarbon.com']
-  },
-  {
-    db: 'PROJECTS_DB',
-    table: 'projects',
-    reportName: 'AI TEAM PROJECTS',
-    recipients: ['mootaz.farwa@avocarbon.com']
+    recipients: ['taha.khiari@avocarbon.com']
   }
 ];
